4193135f9394b242dd86614046c6e75eb5d8a7b2
Fattahur Razzaq <devbyrazzaq@gmail.com>
2024-02-12 00:51:49 +0700

    adding auth base on api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::apiResource('/api-products', ProductController::class)->middleware('auth:sanctum');
+Route::apiResource('/api-categories', CategoryController::class)->middleware('auth:sanctum');
